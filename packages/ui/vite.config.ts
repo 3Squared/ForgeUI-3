bca4795a3ed6a4a5adb724444adc1d51b053b4c2
Jacob Minor <jacob.minor@3squared.com>
2023-10-12 11:47:08 +0100

    Add basic vitest test to pass build

--- packages/ui/vite.config.ts
+++ packages/ui/vite.config.ts
@@ -1,9 +1,23 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import istanbulPlugin from "vite-plugin-istanbul";
+import { join, resolve } from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig(({mode}) => ({
+  resolve: {
+    alias: [
+      {
+        find: "@",
+        replacement: resolve(__dirname, "src")
+      },
+      {
+        find: /~(.+)/,
+        replacement: join(process.cwd(), "node_modules/$1")
+      }
+    ],
+    extensions: [".mjs", ".js", ".ts", ".jsx", ".tsx", ".json", ".vue"]
+  },
   plugins: [
     vue(),
     istanbulPlugin({
@@ -14,4 +28,18 @@ export default defineConfig(({mode}) => ({
       forceBuildInstrument: mode == "test"
     })
   ],
+  test: {
+    alias: [{ find: /^vue$/, replacement: "vue/dist/vue.runtime.common.js" }],
+
+    reporters: ["default", "junit"],
+    outputFile: "test-results/vitest.xml",
+    coverage: {
+      all: true,
+      include: ["src"],
+      exclude: ["**/*.cy.ts", "scripts", "**/*.test.ts"],
+      reportsDirectory: "./coverage/vitest",
+      reporter: ["cobertura", "html", "json"],
+      provider: "v8"
+    }
+  }
 }))
