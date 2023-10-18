6add88e687520b56c46afdb7dfc1eaf05848d62b
Jacob Minor <jacob.minor@3squared.com>
2023-10-18 15:03:37 +0100

    Add remaining characters tests

---
+++ packages/ui/tests/component/remaining-characters/RemainingCharactersWrapper.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <ForgeRemainingCharacters data-cy="remaining-characters" :maxCount="props.maxCount" :currentCount="string.length"/>
+    <InputText data-cy="input-field" v-model="string"/>
+  </div>
+</template>
+
+<script setup lang="ts">
+import ForgeRemainingCharacters from "@/components/ForgeRemainingCharacters.vue";
+import { ref } from "vue";
+import { RemainingCharactersProps } from "../../../src/components/ForgeRemainingCharacters.vue";
+
+const props = defineProps<RemainingCharactersProps>()
+
+
+let string = ref("")
+
+</script>
---
+++ packages/ui/tests/component/remaining-characters/remainingCharacters.cy.ts
@@ -0,0 +1,84 @@
+import RemainingCharactersWrapper from "./RemainingCharactersWrapper.vue";
+import { RemainingCharactersProps } from "@/components/ForgeRemainingCharacters.vue";
+
+function mountRemainingCharacters(props : RemainingCharactersProps) {
+  return cy.mount(RemainingCharactersWrapper, {
+    props
+  })
+}
+
+describe('<ForgeRemainingCharacters />', () => {
+  it('Updates the remaining count when a string is typed', () => {
+    // Arrange
+    const stringToType = "Hello!"
+    const maxCount = 10
+    const expectedRemainingCharacters = 4
+    
+    // Act
+    mountRemainingCharacters({ maxCount: maxCount })
+    
+    cy.get('[data-cy="remaining-characters"]')
+      .should('be.visible')
+      .and('contain.text', maxCount)
+    
+    cy.get('[data-cy="input-field"]').type(stringToType)
+    
+    // Assert
+    cy.get('[data-cy="remaining-characters"]')
+      .should('be.visible')
+      .and('contain.text', expectedRemainingCharacters)
+  })
+  
+  it("Makes text red once the string length exceeds the max count", () => {
+    // Arrange
+    const stringToType = "Hello!"
+    const maxCount = 1
+    const expectedRemainingCharacters = -5
+    const expectedClass = "text-danger"
+
+    // Act
+    mountRemainingCharacters({ maxCount: maxCount })
+
+    cy.get('[data-cy="remaining-characters"]')
+      .should('be.visible')
+      .and('contain.text', maxCount)
+
+    cy.get('[data-cy="input-field"]').type(stringToType)
+
+    // Assert
+    cy.get('[data-cy="remaining-characters"]')
+      .should('be.visible')
+      .and('contain.text', expectedRemainingCharacters)
+      .and("have.class", expectedClass)
+  })
+  
+  it("Removes danger class once string length is less than the max count", () => {
+    // Arrange
+    const stringToType = "Hello!"
+    const maxCount = 1
+    const expectedRemainingCharacters = -5
+    const expectedClass = "text-danger"
+
+    // Act
+    mountRemainingCharacters({ maxCount: maxCount })
+
+    cy.get('[data-cy="remaining-characters"]')
+      .should('be.visible')
+      .and('contain.text', maxCount)
+
+    cy.get('[data-cy="input-field"]').type(stringToType)
+    
+    cy.get('[data-cy="remaining-characters"]')
+      .should('be.visible')
+      .and('contain.text', expectedRemainingCharacters)
+      .and("have.class", expectedClass)
+
+    cy.get('[data-cy="input-field"]').clear()
+    
+    // Assert
+    cy.get('[data-cy="remaining-characters"]')
+      .should('be.visible')
+      .and('contain.text', maxCount)
+      .and("not.have.class", expectedClass)
+  })
+})
