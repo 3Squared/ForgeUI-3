4181e6bfc0be55851ce6549a369b1c4636c9073b
Jacob Minor <jacob.minor@3squared.com>
2023-10-18 14:38:42 +0100

    Add remaining character component

---
+++ packages/ui/src/components/ForgeRemainingCharacters.vue
@@ -0,0 +1,20 @@
+<template>
+  <div :class="remainingCount < 0 ? 'text-danger' : ''">Remaining characters: {{ remainingCount }}</div>
+</template>
+
+<script setup lang="ts">
+import { computed } from "vue"
+
+const props = defineProps({
+  maxCount: {
+    type: Number,
+    required: true
+  },
+  currentCount: {
+    type: Number,
+    required: true
+  }
+})
+
+const remainingCount = computed(() => props.maxCount - props.currentCount);
+</script>
