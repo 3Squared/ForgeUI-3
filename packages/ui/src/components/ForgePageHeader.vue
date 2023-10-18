4ec56a8f77bbb18377f5d902c73a07c889e7733a
Jacob Minor <jacob.minor@3squared.com>
2023-10-18 16:02:35 +0100

    Add Forge Page Header component

---
+++ packages/ui/src/components/ForgePageHeader.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="d-flex justify-content-between pb-2">
+    <h1 class="h3">{{ title }}</h1>
+    <div class="d-flex align-items-center">
+      <slot />
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+/**
+ *   The Forge Page Header is a component that you can put at the top of every page with the title on the left and the buttons on the right
+ * @displayName Page Header
+ **/
+defineProps<ForgePageHeaderProps>();
+
+export interface ForgePageHeaderProps {
+  title: string
+}
+</script>
