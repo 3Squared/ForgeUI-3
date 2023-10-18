56681b71bb4f970c02e2e8722850e218ffd832ff
Jacob Minor <jacob.minor@3squared.com>
2023-10-18 11:20:48 +0100

    Add cypress tests for Forge Checkbox and refactor file structure

--- packages/ui/cypress/support/commands.ts
+++ packages/ui/cypress/support/commands.ts
@@ -35,15 +35,3 @@
 //     }
 //   }
 // }
-
-import { mount } from 'cypress/vue';
-
-declare global {
-  namespace Cypress {
-    interface Chainable {
-      mount: typeof mount;
-    }
-  }
-}
-
-Cypress.Commands.add('mount', mount);
