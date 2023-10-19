2ffac722a6874c227f8cddaf07baab09c3958e8e
Jacob Minor <jacob.minor@3squared.com>
2023-10-19 13:09:09 +0100

    Add forge action button and tests

--- packages/ui/components.d.ts
+++ packages/ui/components.d.ts
@@ -7,7 +7,9 @@ export {}
 
 declare module 'vue' {
   export interface GlobalComponents {
+    Button: typeof import('primevue/button')['default']
     Checkbox: typeof import('primevue/checkbox')['default']
+    ForgeActionButton: typeof import('./src/components/ForgeActionButton.vue')['default']
     ForgeCheckbox: typeof import('./src/components/ForgeCheckbox.vue')['default']
     ForgeRemainingCharacters: typeof import('./src/components/ForgeRemainingCharacters.vue')['default']
     ForgeToast: typeof import('./src/components/ForgeToast.vue')['default']
---
+++ packages/ui/src/components/ForgeActionButton.vue
@@ -0,0 +1,35 @@
+<template>
+  <Button v-bind="{...props, ...$attrs}" :loading="loading" :disabled="loading || props.disabled" @click="performAction"  />
+  <Toast />
+</template>
+
+<script setup lang="ts">
+  import { ButtonProps } from "primevue/button";
+  import { ref } from "vue";
+  import { useToast } from "primevue/usetoast";
+
+  const loading = ref<boolean>(false)
+
+  export interface ForgeActionButtonProps extends /* @vue-ignore */ ButtonProps {
+    errorMessage?: string,
+    action: Function,
+    params?: Array<any>
+  }
+  
+  const props = withDefaults(defineProps<ForgeActionButtonProps>(), {
+    errorMessage: "This action failed",
+    params: Array
+  })
+  
+  const toasts = useToast()
+  
+  const performAction = async () => {
+    loading.value = true
+    try {
+      await props.action.apply(this, props.params)
+    } catch(e : any) {
+      toasts.add({ severity: "error", summary: typeof e === "string" ? e : props.errorMessage})
+    }
+    loading.value = false
+  }
+</script>
