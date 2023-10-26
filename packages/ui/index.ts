e5b702fcd456e97124b1b834cbba45f38b4df8ef
Jacob Minor <jacob.minor@3squared.com>
2023-10-26 09:44:05 +0100

    Update forge types file to be a ts file instead of a d.ts file

--- packages/ui/index.ts
+++ packages/ui/index.ts
@@ -1,5 +1,7 @@
 import Bootstrap_PT from "./src/Bootstrap_PT"
+import '@/types/forge-type.ts'
 import '@/styles/main.scss'
+
 import ForgeCheckbox from "@/components/ForgeCheckbox.vue";
 import ForgeToast from '@/components/ForgeToast.vue';
 
