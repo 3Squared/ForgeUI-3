d652ba217390fd28324008f349580ad382fa70d3
Jacob Minor <jacob.minor@3squared.com>
2023-11-06 11:25:03 +0000

    Remove v-model and change to value

--- packages/ui/src/components/ForgeFormField.vue
+++ packages/ui/src/components/ForgeFormField.vue
@@ -2,10 +2,10 @@
   <div class="d-flex flex-column">
     <label :for="props.name" v-if="props.type !== 'checkbox'">{{ props.label }}</label>
     <forge-checkbox :id="props.name" v-if="props.type === 'checkbox'" :rules="rules" :label="props.label" v-bind="$attrs" />
-    <input-number :id="props.name" v-else-if="props.type === 'number'" :placeholder="props.placeholder" v-model="value" :input-class="{'is-invalid': hasErrors }" :class="{'is-invalid': hasErrors }" v-bind="$attrs" />
-    <Textarea :id="props.name" v-else-if="props.type === 'textarea'" :placeholder="props.placeholder" v-model="value" :class="{'is-invalid': hasErrors }" v-bind="$attrs" />
-    <InputMask :id="props.name" v-else-if="props.type === 'mask'" :placeholder="props.placeholder" v-model="value" :mask="props.mask" :class="{'is-invalid': hasErrors }" v-bind="$attrs"/>
-    <InputText :id="props.name" v-else-if="props.type === 'text'" placeholder="text" v-model="value" :class="{'is-invalid': hasErrors }" v-bind="$attrs" />
+    <input-number :id="props.name" v-else-if="props.type === 'number'" :placeholder="props.placeholder" :value="value" :input-class="{'is-invalid': hasErrors }" :class="{'is-invalid': hasErrors }" v-bind="$attrs" />
+    <Textarea :id="props.name" v-else-if="props.type === 'textarea'" :placeholder="props.placeholder" :value="value" :class="{'is-invalid': hasErrors }" v-bind="$attrs" />
+    <InputMask :id="props.name" v-else-if="props.type === 'mask'" :placeholder="props.placeholder" :value="value" :mask="props.mask" :class="{'is-invalid': hasErrors }" v-bind="$attrs"/>
+    <InputText :id="props.name" v-else-if="props.type === 'text'" placeholder="text" :value="value" :class="{'is-invalid': hasErrors }" v-bind="$attrs" />
     <small v-if="props.type !== 'checkbox'" data-cy="error" class="invalid-feedback">{{ errorMessage || '&nbsp;'}}</small>
   </div>
 </template>
