8d4a2bb0081878c7985a73b29e41988d9a30c6b6
Jacob Minor <jacob.minor@3squared.com>
2023-11-21 08:38:34 +0000

    Add tooltip tests

--- packages/ui/tests/component/tooltip/TooltipWrapper.vue
+++ packages/ui/tests/component/tooltip/TooltipWrapper.vue
@@ -1,12 +1,20 @@
 <template>
   <div class="d-flex flex-wrap vh-100 justify-content-center align-items-center">
-    <InputText v-tooltip.left="'I am a tooltip'" class="w-25"/>
+    <InputText :id="props.id" v-tooltip.focus="'I am a tooltip'" class="w-25" v-if="props.position === undefined"/>
+    <InputText :id="props.id" v-tooltip.focus.left="'I am a tooltip'" class="w-25" v-else-if="props.position === 'left'" />
+    <InputText :id="props.id" v-tooltip.focus.ight="'I am a tooltip'" class="w-25" v-else-if="props.position === 'right'" />
+    <InputText :id="props.id" v-tooltip.focus.bottom="'I am a tooltip'" class="w-25" v-else-if="props.position === 'bottom'" />
+    <InputText :id="props.id" v-tooltip.focus.top="'I am a tooltip'" class="w-25" v-else-if="props.position === 'top'" />
+
   </div>
 </template>
 
 <script setup lang="ts">
 
 export interface TooltipWrapperProps {
-  position: 'top' | 'bottom' | 'left' | 'right'
+  position: 'top' | 'bottom' | 'left' | 'right' | undefined,
+  id: string
 }
+
+const props = defineProps<TooltipWrapperProps>();
 </script>
--- packages/ui/tests/component/tooltip/tooltip.cy.ts
+++ packages/ui/tests/component/tooltip/tooltip.cy.ts
@@ -2,12 +2,13 @@
 import TooltipWrapper, {TooltipWrapperProps} from "./TooltipWrapper.vue";
 
 const inputId = "input"
+const tooltipId = '[data-pc-name="tooltip"]'
 function mountTooltip(props : TooltipWrapperProps){
   cy.mount(TooltipWrapper, {
-    props,
-    attrs: {
+    props: {
+      ...props, 
       id: inputId
-    }
+    },
   })
 }
 
@@ -15,11 +16,46 @@ describe("<Tooltip />", () => {
   it("Mounts", () => {
     // Act
     mountTooltip({ position: 'right', })
-    cy.get(`#${inputId}`).realHover()
+    cy.get(`#${inputId}`).focus()
 
     // Assert
-    cy.get('[data-pc-name="tooltip"]')
+    cy.get(tooltipId)
         .should('exist')
         .and('be.visible')
+  })
+  
+  ;[{ title: 'Displays the tooltip on the right when direction is undefined.', position: undefined },
+    { title: 'Displays the tooltip on the right when the direction is right', position: 'right' },
+    { title: 'Displays the tooltip on the left when the direction is left', position: 'left' },
+    { title: 'Displays the tooltip on top when the direction is top', position: 'top' },
+    { title: 'Displays the tooltip on the bottom when the direction is bottom', position: 'bottom' }
+  ].forEach(({ title, position}) => {
+    
+    function getPositionClass(position : string | undefined) {
+      switch (position) {
+        case undefined:
+        case 'right':
+          return 'bs-tooltip-end'
+        case 'left':
+          return 'bs-tooltip-start'
+        case 'top':
+          return 'bs-tooltip-top'
+        case 'bottom': 
+          return 'bs-tooltip-bottom'
+      }
+    }
+    
+    it(title, () => {
+      // Arrange
+      const expectedClass = getPositionClass(position)
+      
+      // Act
+      mountTooltip({ position: position })
+      cy.get(`#${inputId}`).focus()
+
+      // Assert
+      cy.get(tooltipId)
+        .should('have.class', expectedClass)
+    })
   })
 });
