8057cbdda2d0a5b305f92a0d180b05dcb0efc3ff
Jacob Minor <jacob.minor@3squared.com>
2024-03-04 15:30:15 +0000

    Fix lint issues

--- apps/docs/src/pages/index.vue
+++ apps/docs/src/pages/index.vue
@@ -5,11 +5,11 @@
       <img src="https://cdn.dev.3sq.app/assets/products/general/images/forge-favicon.ico" alt="" class="ms-2" />
     </h2>
     <div class="d-flex mt-3 text-center mx-5">
-        <span class="fw-bold">ForgeUI </span>is designed to be a wrapper around different libraries that provide a consistent UI style. It is comprised of many different packages that have been
-        combined to create a one stop shop for UI.
+      <span class="fw-bold">ForgeUI</span>
+      is designed to be a wrapper around different libraries that provide a consistent UI style. It is comprised of many different packages that have been
+      combined to create a one stop shop for UI.
     </div>
   </div>
 </template>
 
-<script setup lang="ts">
-</script>
+<script setup lang="ts"></script>
