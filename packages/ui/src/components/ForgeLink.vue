3a23db24cd58dca5afd3a604065614cccda8612f
Jacob Minor <jacob.minor@3squared.com>
2024-03-25 16:43:59 +0000

    Refactor forge link

--- packages/ui/src/components/ForgeLink.vue
+++ packages/ui/src/components/ForgeLink.vue
@@ -1,21 +1,27 @@
 <template>
   <a 
       id="link"
-      class="cursor-pointer"
-      :class="classes"
+      class="cursor-pointer d-flex"
+      :href="url"
+      :class="linkClasses"
   >
-  <slot>{{ label }}</slot>
-</a>
+    <slot>
+      <Icon id="link-icon" :icon="iconName" class="bi my-auto" :class="iconClasses" v-if="iconName"/>
+      {{ label }}
+    </slot>
+  </a>
 </template>
 
 <script setup lang="ts">
+import { Icon } from '@iconify/vue'
 import { computed, defineProps } from "vue";
 import { Severity } from "../types/forge-types";
 
-type LinkOpacity = '10' | '25' | '50' | '75' | '100'
-type UnderlineOffset = '1' | '2' | '3'
+export type LinkOpacity = '10' | '25' | '50' | '75' | '100'
+export type UnderlineOffset = '1' | '2' | '3'
 
 export interface ForgeLinkProps {
+  url: string,
   opacity?: LinkOpacity,
   severity?: Severity,
   underlineSeverity?: Severity,
@@ -23,8 +29,10 @@ export interface ForgeLinkProps {
   underlineOpacity?: '0' | LinkOpacity,
   hoverOpacity?: LinkOpacity,
   underlineHoverOpacity?: LinkOpacity,
+  label?: string,
   animateIconOnHover?: boolean,
-  label?: string
+  positionIconEnd?: boolean,
+  iconName?: string
 }
 
 const {
@@ -35,18 +43,28 @@ const {
   underlineOpacity,
   animateIconOnHover,
   underlineOffset,
-  underlineSeverity
+  underlineSeverity,
+  positionIconEnd,
+  url
 } = defineProps<ForgeLinkProps>()
 
-const classes = computed<Object>(() => ({
+const linkClasses = computed<Object>(() => ({
   [`link-${severity}`]: severity,
+  [`link-underline-${underlineSeverity}`]: underlineSeverity,
   [`link-opacity-${opacity}`]: opacity,
   [`link-opacity-${hoverOpacity}-hover`]: hoverOpacity,
-  [`link-underline-${underlineSeverity}`]: underlineSeverity,
-  [`link-underline-opacity-${underlineOpacity}`]: underlineOpacity,
-  [`link-underline-opacity-${underlineHoverOpacity}-hover`]: underlineHoverOpacity,
+  [`link-underline-opacity-${underlineOpacity ?? opacity}`]: underlineOpacity || opacity,
+  [`link-underline-opacity-${underlineHoverOpacity ?? hoverOpacity}-hover`]: underlineHoverOpacity || hoverOpacity,
   [`link-offset-${underlineOffset}`]: underlineOffset,
-  'icon-link icon-link-hover': animateIconOnHover  
+  'icon-link icon-link-hover': animateIconOnHover,
+}))
+
+const iconClasses = computed<Object>(() => ({
+  'order-2': positionIconEnd,
+  'me-1': !positionIconEnd && !animateIconOnHover,
+  'ms-1': positionIconEnd && !animateIconOnHover,
+  'ms-0': positionIconEnd && animateIconOnHover,
+  'me-0': animateIconOnHover
 }))
 
 </script>
