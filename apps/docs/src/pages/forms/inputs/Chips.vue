56b7c05852851c5e1f05e325ec918d95b8a1c104
Jacob Minor <jacob.minor@3squared.com>
2024-06-03 10:28:06 +0100

    Fix lint issues

--- apps/docs/src/pages/forms/inputs/Chips.vue
+++ apps/docs/src/pages/forms/inputs/Chips.vue
@@ -20,19 +20,22 @@ import { Playground, usePlayground } from "@3squared/forge-playground-3";
 import { computed } from "vue";
 import { severities } from "../../../composables/playgroundOptions";
 
-const { options, propVals, config, reset } = usePlayground({
-  name: "",
-  chipSeverity: severities[0],
-  separator: ",",
-  pill: false,
-  max: 9999,
-  addOnBlur: false,
-  allowDuplicate: true,
-  disabled: false,
-  placeholder: ""
-}, {
-  chipSeverity: { type: "select", options: severities }
-});
+const { options, propVals, config, reset } = usePlayground(
+  {
+    name: "",
+    chipSeverity: severities[0],
+    separator: ",",
+    pill: false,
+    max: 9999,
+    addOnBlur: false,
+    allowDuplicate: true,
+    disabled: false,
+    placeholder: ""
+  },
+  {
+    chipSeverity: { type: "select", options: severities }
+  }
+);
 
 const code = computed(() => `<ForgeChips${propVals.value.length > 0 ? " " + propVals.value.join(" ") : ""} />`);
 </script>
