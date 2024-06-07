02fbca14e7c659d7270e8c4b9157078e4cc47d88
Jacob Minor <jacob.minor@3squared.com>
2024-06-07 15:17:27 +0100

    Fix lint issues

--- apps/docs/src/pages/forms/Switch.vue
+++ apps/docs/src/pages/forms/Switch.vue
@@ -16,19 +16,17 @@
 
 <script setup lang="ts">
 import { ForgePageHeader } from "@3squared/forge-ui-3";
-import InputSwitch from 'primevue/inputswitch'
+import InputSwitch from "primevue/inputswitch";
 import { Playground, usePlayground } from "@3squared/forge-playground-3";
 import { computed, ref } from "vue";
 
-const { options, propVals, config, reset } = usePlayground(
-    {
-      trueValue: "true",
-      falseValue: "false",
-      disabled: false,
-      readonly: false,
-      tabindex: 0
-    }
-);
+const { options, propVals, config, reset } = usePlayground({
+  trueValue: "true",
+  falseValue: "false",
+  disabled: false,
+  readonly: false,
+  tabindex: 0
+});
 
 const value = ref<boolean>(false);
 
