e13cac081f5cbea1256b36794ebf964a745462d2
Isabel Mallon <isabel.mallon@3squared.com>
2025-01-07 12:06:40 +0000

    Update package lock

--- apps/components-testing/src/components/ExampleButton.vue
+++
@@ -1,8 +0,0 @@
-<template>
-  <Button label="Test" badge="8" />
-  <Button label="Test" badge="8" severity="secondary"/>
-</template>
-
-<script setup lang="ts">
-import Button from "primevue/button"
-</script>
