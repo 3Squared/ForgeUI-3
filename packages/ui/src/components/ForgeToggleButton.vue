1ec4b534cf2e324ebf404e66594df71f885727e1
Isabel Mallon <isabel.mallon@3squared.com>
2025-03-25 14:49:26 +0000

    Add z-index to button group

--- packages/ui/src/components/ForgeToggleButton.vue
+++ packages/ui/src/components/ForgeToggleButton.vue
@@ -22,8 +22,8 @@ const pt = computed(() => ({
         class: [
           'btn',
           {
-            [`focus-ring focus-ring-${props.severity} btn-${props.severity}-subtle border border-2 border-${props.severity} fw-500 text-${props.severity}`]: options.context.active,
-            [`border text-${props.severity} select-btn-${props.severity}`]: !options.context.active,
+            [`focus-ring focus-ring-${props.severity} btn-${props.severity}-subtle border border-2 border-${props.severity} fw-500 text-${props.severity} z-1`]: options.context.active,
+            [`border text-${props.severity} select-btn-${props.severity} z-0`]: !options.context.active,
             ['btn-secondary-subtle']: options.context.disabled,
             ['border-danger']: options.props.invalid
           },
